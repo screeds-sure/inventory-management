323610f69c388fac896ff36299c107336556e983
Jackson Miller <153257334+screeds-sure@users.noreply.github.com>
2023-12-08 12:03:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+from product import Product
+from order import Order
+from inventory import Inventory
+
+# Sample usage
+inventory = Inventory()
+
+laptop = Product("P1", "Laptop", 999.99, 10)
+phone = Product("P2", "Smartphone", 499.99, 20)
+
+inventory.add_product(laptop)
+inventory.add_product(phone)
+
+order_products = [laptop, phone]
+order = Order("O1", order_products, "John Doe")
+
+print("Inventory:")
+inventory.display_inventory()
+
+print("\nPlacing Order:")
+print(order)
 
